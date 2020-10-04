ef466a1a4cca31a6e57e1eb125f47350c0e4da68
CK <cheekhuin.loong@gmail.com>
2020-10-04 15:53:30 +0800

    feat: added an openapi.yaml/.yml path feature closes #15
    path to get specs in yaml
    
    test: Added test for openapi.yaml/yml path feature

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { get: getSchema, set: setSchema } = require('./lib/layer-schema')
 const minimumViableDocument = require('./lib/minimum-doc')
 const generateDocument = require('./lib/generate-doc')
 const defaultRoutePrefix = '/openapi'
+const YAML = require('yaml')
 
 module.exports = function ExpressOpenApi (_routePrefix, _doc, _opts) {
   // Acceptable arguments:
@@ -134,6 +135,16 @@ module.exports = function ExpressOpenApi (_routePrefix, _doc, _opts) {
     middleware.document = generateDocument(middleware.document, req.app._router || req.app.router)
     res.json(middleware.document)
   })
+
+  // OpenAPI document as yaml
+  router.get([`${routePrefix}.yaml`, `${routePrefix}.yml`], (req, res) => {
+    const jsonSpec = generateDocument(middleware.document, req.app._router || req.app.router)
+    const yamlSpec = YAML.stringify(jsonSpec)
+
+    res.type('yaml')
+    res.send(yamlSpec)
+  })
+
   router.get(`${routePrefix}/components/:type/:name.json`, (req, res, next) => {
     const { type, name } = req.params
     middleware.document = generateDocument(middleware.document, req.app._router || req.app.router)
